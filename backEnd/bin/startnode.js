e6f022caa074aebd7f466fdf8387568779b7e441
RabbitTank <Mikael.Petrow@student.lamk.fi>
2019-10-31 15:01:15 +0200

    31.10.2019 pusku

--- backEnd/bin/startnode.js
+++ backEnd/bin/startnode.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
 const express = require('express')
 const app = express()
-
+//DB CONSTS
+const Item = require('../models/items');
 // this starting code for node
 
 // Create routes
-let routes = require('../routes/routes.js');
+const routes = require('../routes/routes.js');
 
-// Make app use the
+// Make app use the routes
 app.use(routes)
 
 // To serve static content (images, html pages etc..)
@@ -16,6 +17,32 @@ app.use(express.static('public'))
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.info(`Server has started on port: ${PORT}`))
 
+// DB THINGS
+mongoose.connect('mongodb://localhost:27017/testdb',{ useNewUrlParser: true });
+mongoose.connection.once('open',function(){
+    console.log('Connection has been made!'); 
+}).on('error',function(error){
+    console.log('Connection error:', error);
+});
 
-// API endPoints == Routes in Express
+
+var item = new Item({
+    name: 'A thing',
+    weight: '5'
+});
+item.save().then(function(){
+    console.log("Person was saved");
+});
+
+var item2 = new Item({
+    name: 'Nothing thing',
+    weight: '51'
+});
+item2.save().then(function(){
+    console.log("Person was saved");
+});
+
+// Item.collection.drop();
+
+
 
