8eb68888ffc3d7ee85ad4d562ac778d091e68b54
Pauli <pauli.sairanen@gmail.com>
2020-02-20 17:08:47 +0200

    Created a working HTTPS get function for speakers component

--- frontEnd/screens/event/SpeakersScreen.js
+++ frontEnd/screens/event/SpeakersScreen.js
@@ -1,26 +1,61 @@
-import React from 'react'
-import { View, FlatList } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { View, FlatList, ActivityIndicator } from 'react-native'
 
-import speakersData from '../../data/jsonFiles/speakers.json'
+import Colors from '../../constants/Colors'
+
+// import speakersData from '../../data/jsonFiles/speakers.json'
 import SpeakersItem from '../../components/SpeakersItem'
 
 const SpeakersScreen = props => {
-  return (
-    <View>
-      <FlatList
-        data={speakersData}
-        keyExtractor={(item, index) => index.toString()}
-        renderItem={speakersData =>
-          <SpeakersItem 
-            speaker={speakersData.item.Speaker}
-            title={speakersData.item.Title}
-            company={speakersData.item.Company}
-            specialTitle={speakersData.item.SpecialTitle}
-          />
-        }
-      />
-    </View>
-  )
+  const [dataFromServer, setDataFromServer] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+
+  //__________ Function that gets data from the server __________
+  const fetchData = async () => {
+    setIsLoading(true)
+    try {
+      let response = await fetch('https://sahat.lamk.fi/findSpeakers')
+      let responseJson = await response.json()
+      
+      // DB returns an array with a key named speakers
+      setDataFromServer(responseJson.speakers)
+      setIsLoading(false)
+      console.log(responseJson)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  // Calls the function only once, when screen is laucnhed
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  if (isLoading === true) {
+    return (
+      <View>
+        <ActivityIndicator size='large' color={Colors.primary} />
+      </View>
+    )
+  } else {
+    return (
+      <View>
+        <FlatList
+          data={dataFromServer}
+          extraData={dataFromServer}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={speakersData =>
+            <SpeakersItem
+              speaker={speakersData.item.Speaker}
+              title={speakersData.item.Title}
+              company={speakersData.item.Company}
+              specialTitle={speakersData.item.SpecialTitle}
+            />
+          }
+        />
+      </View>
+    )
+  }
 }
 
 export default SpeakersScreen
