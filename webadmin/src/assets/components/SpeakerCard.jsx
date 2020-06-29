ef52125943e4636fe461ba893bda3ca1761218a7
Simo Wesa <simo.wesa@gmail.com>
2020-06-29 15:30:11 +0300

    fix

--- webadmin/src/assets/components/SpeakerCard.jsx
+++ webadmin/src/assets/components/SpeakerCard.jsx
@@ -47,7 +47,7 @@ const SpeakerCard = props => {
       <Form>
         <FormGroup className="file">
           <FormLabel><Image className="filePrev" src={formObject.speakerImgsrc} roundedCircle/></FormLabel>
-          <label htmlFor={'hidden-'+props.index} id="lableForHidden">Choose file</label>
+          <label htmlFor={'hidden-'+props.index} id="lableForHidden">Choose image</label>
           <FormControl size="sm" onChange={(e) => {changeHandler(e); fileHandler(e); changeImage(e)}} id={'hidden-'+props.index} className="hidden" type='file' name="ImageID"></FormControl>
           {/* <Form.File size="sm" onChange={(e) => {changeHandler(e); fileHandler(e); changeImage(e)}} name="ImageID"/> */}
         </FormGroup>
