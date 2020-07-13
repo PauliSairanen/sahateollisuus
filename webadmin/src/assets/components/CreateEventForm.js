f8e18c73c1a76db32584fe61a0997211a50447aa
Simo Wesa <simo.wesa@gmail.com>
2020-07-13 08:46:34 +0300

    Update CreateEventForm.js
    test json removed

--- webadmin/src/assets/components/CreateEventForm.js
+++ webadmin/src/assets/components/CreateEventForm.js
@@ -611,9 +611,9 @@ const CreateEventForm = (props) => { // Todo rename to CreateEventScreen
                 uploadFile(e.target.files[0],"test")
             }}/> */}
             
-            <p>{Changes ? "Are Changes" : "No Changes"}</p>
+            {/* <p>{Changes ? "Are Changes" : "No Changes"}</p>
             <p>{JSON.stringify(finalForm, null, 2)}</p>
-            <p>{JSON.stringify(Files,null,2)}</p>
+            <p>{JSON.stringify(Files,null,2)}</p> */}
         </div>
     )
 }
