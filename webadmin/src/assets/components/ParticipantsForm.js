b5f3af233b06668ac9565e817c7bceaa74d147c8
Simo Wesa <simo.wesa@gmail.com>
2020-07-13 15:20:11 +0300

    Update ParticipantsForm.js
    fixed clear text

--- webadmin/src/assets/components/ParticipantsForm.js
+++ webadmin/src/assets/components/ParticipantsForm.js
@@ -165,7 +165,7 @@ const ParticipantsForm = (props) => {
                     <InputGroup style={{display:'flex', justifyContent:'center', alignItems:'center', borderRadius:'25px', border:'1.5px solid lightgrey', padding:'0px', marginRight: '10px'}}>
                         <FormControl className="noActive" onKeyDown={(e)=>{if(e.keyCode === 13) setSearch(e.target.value)}}/>
                     </InputGroup>
-                    <Button className="otherButtons" onClick={(e)=>{e.target.parentNode.parentNode.childNodes[0].value=""; setSearch("")}}>Clear</Button>
+                    <Button className="otherButtons" onClick={(e)=>{e.target.parentNode.childNodes[0].childNodes[0].value = ""; setSearch("")}}>Clear</Button>
                 <Button className="otherButtons" onClick={(e)=>{setSearch(e.currentTarget.parentNode.childNodes[0].value)}}>Search</Button>
             </div>
         </div>
