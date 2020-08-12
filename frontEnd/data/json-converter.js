0f85ecff6cb6dca959df9b148592f245207f39a6
Pauli <pauli.sairanen@gmail.com>
2020-08-12 15:58:37 +0300

    Deleted Data folder from project

--- frontEnd/data/json-converter.js
+++
@@ -1,8 +0,0 @@
-let csvToJson = require('convert-csv-to-json');
- 
-let fileInputName = 'speakers.csv'; 
-let fileOutputName = 'speakers.json';
-
-csvToJson.fieldDelimiter(',') .getJsonFromCsv(fileInputName)
- 
-csvToJson.generateJsonFileFromCsv(fileInputName,fileOutputName);
